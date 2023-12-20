9441296e6b1d2decf59c5d1bfcdf3b6bd7599271
MeseryGG <Meserygamer@gmail.com>
2023-12-20 17:24:24 +0300

    Добавлены кнопки просмотра расписания на сегодня и отписки от рассылки

---
+++ CallBackVKAPI/Controllers/ButtonsBuilders/ShowTodayScheduleButtonBuilder.cs
@@ -0,0 +1,31 @@
+﻿using VkNet.Model;
+
+namespace CallBackVKAPI.Controllers.ButtonsBuilders
+{
+    public class ShowTodayScheduleButtonBuilder : IButtonBuilder
+    {
+        public ShowTodayScheduleButtonBuilder() => _button = new();
+
+
+        public IButtonBuilder SetAction()
+        {
+            _button.Action = new MessageKeyboardButtonAction()
+            {
+                Type = VkNet.Enums.StringEnums.KeyboardButtonActionType.Text,
+                Label = "Посмотреть расписание на сегодня",
+                Payload = "{\"button\": \"4\"}"
+            };
+            return this;
+        }
+        public IButtonBuilder SetColor()
+        {
+            _button.Color = VkNet.Enums.StringEnums.KeyboardButtonColor.Secondary;
+            return this;
+        }
+
+        public MessageKeyboardButton Build() => _button;
+
+
+        private MessageKeyboardButton _button;
+    }
+}
---
+++ CallBackVKAPI/Controllers/ButtonsBuilders/UnSubscribeOnUpdateButtonBuilder.cs
@@ -0,0 +1,31 @@
+﻿using VkNet.Model;
+
+namespace CallBackVKAPI.Controllers.ButtonsBuilders
+{
+    public class UnSubscribeOnUpdateButtonBuilder : IButtonBuilder
+    {
+        public UnSubscribeOnUpdateButtonBuilder() => _button = new();
+
+
+        public IButtonBuilder SetAction()
+        {
+            _button.Action = new MessageKeyboardButtonAction()
+            {
+                Type = VkNet.Enums.StringEnums.KeyboardButtonActionType.Text,
+                Label = "Отписаться от рассылки",
+                Payload = "{\"button\": \"3\"}"
+            };
+            return this;
+        }
+        public IButtonBuilder SetColor()
+        {
+            _button.Color = VkNet.Enums.StringEnums.KeyboardButtonColor.Negative;
+            return this;
+        }
+
+        public MessageKeyboardButton Build() => _button;
+
+
+        private MessageKeyboardButton _button;
+    }
+}
