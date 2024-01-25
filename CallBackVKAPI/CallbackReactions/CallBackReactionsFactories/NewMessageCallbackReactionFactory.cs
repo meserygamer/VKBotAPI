920ce093558d36f7aa6b35f84715ce4132a94b75
MeseryGG <Meserygamer@gmail.com>
2024-01-26 02:52:13 +0300

    Рефакторинг логгера для NewMessageCallbackReactionFactory

--- CallBackVKAPI/CallbackReactions/CallBackReactionsFactories/NewMessageCallbackReactionFactory.cs
+++ CallBackVKAPI/CallbackReactions/CallBackReactionsFactories/NewMessageCallbackReactionFactory.cs
@@ -1,6 +1,8 @@
 ﻿using CallBackVKAPI.Controllers.BotCommands;
 using CallBackVKAPI.Controllers.CallbackReactions;
 using CallBackVKAPI.Logger;
+using CallBackVKAPI.Logger.CallBackReactionFactoriesLoggers;
+using CallBackVKAPI.Logger.ManagersLoggers;
 using CallBackVKAPI.Models;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
@@ -16,19 +18,34 @@ namespace CallBackVKAPI.Controllers.CallBackReactionsFactories
     {
         public NewMessageCallbackReactionFactory(IVkApi vkApi, Updates update, IFileLogger logger) 
         {
-            _logger = logger;
-            _newMessageCallbackReactionFactoryLogger = new NewMessageCallbackReactionFactoryLogger(_logger);
+            Logger = new NewMessageCallbackReactionFactoryLogger(logger);
             string messageStringForm = update.Object["message"].AsObject().ToJsonString();
-            _messageFromUser = JsonConvert.DeserializeObject<Message>(messageStringForm);
-            if (_messageFromUser is null)
+            MessageFromUser = JsonConvert.DeserializeObject<Message>(messageStringForm);
+            if (MessageFromUser is null)
             {
-                _newMessageCallbackReactionFactoryLogger.LogMessageDeserializeError();//Логгирование ошибки
-                throw new ArgumentNullException("Преобразование Object:Message в тип Message не удалось");
+                Exception ex = new ArgumentNullException("Object:Message", "Преобразование Object:Message в тип Message не удалось");
+                Logger.LogException(ex);//Логгирование ошибки
+                throw ex;
             }
-            _vkApi = vkApi;
-            _newMessageCallbackReactionFactoryLogger.LogInfoAboutCreatingObject(); 
-            //Логгирование создания фабрики
+            VkApi = vkApi;
+            Logger.LogObjectCreation(typeof(NewMessageCallbackReactionFactory)); //Логгирование создания фабрики
         }
+
+
+        /// <summary>
+        /// Сообщение полученное из CallbackAPI
+        /// </summary>
+        public Message MessageFromUser { get; private init; }
+
+        /// <summary>
+        /// Экземпляр VK API
+        /// </summary>
+        public IVkApi VkApi { get; private init; }
+
+        /// <summary>
+        /// Логгер для данного класса
+        /// </summary>
+        public NewMessageCallbackReactionFactoryLogger Logger { get; private set; }
 
 
         /// <summary>
@@ -38,8 +55,7 @@ namespace CallBackVKAPI.Controllers.CallBackReactionsFactories
         public ICallBackReaction CreateCallbackReaction()
         {
             IBotCommand botCommand = CreateBotCommand();
-            _newMessageCallbackReactionFactoryLogger.LogInfoAboutCreatingCallbackReaction(); 
-            //Логгирование создания реакции нового сообщения
+            Logger.LogReactionCreating(typeof(NewMessageCallbackReaction)); //Логгирование создания реакции нового сообщения
             return new NewMessageCallbackReaction(botCommand);
         }
 
@@ -50,84 +66,8 @@ namespace CallBackVKAPI.Controllers.CallBackReactionsFactories
         /// <returns>Команда обернутая в IBotCommand</returns>
         private IBotCommand CreateBotCommand()
         {
-            _newMessageCallbackReactionFactoryLogger.LogInfoAboutCreatingCallbackReactionCommand(); 
-            //Логгирование создания комманды для реакции
-            return new BotCommandManager(_vkApi, _messageFromUser, _logger).GetBotCommand();
+            Logger.LogCommandCreating(); //Логгирование создания комманды для реакции
+            return new BotCommandManager(VkApi, MessageFromUser, Logger.FileLogger).GetBotCommand();
         }
-
-
-        /// <summary>
-        /// Сообщение полученное из CallbackAPI
-        /// </summary>
-        private Message _messageFromUser;
-
-        /// <summary>
-        /// Экземпляр vkApi
-        /// </summary>
-        private IVkApi _vkApi;
-
-        /// <summary>
-        /// Используемый файловый логгер
-        /// </summary>
-        private IFileLogger _logger;
-
-        /// <summary>
-        /// Логгер для фабрики
-        /// </summary>
-        private NewMessageCallbackReactionFactoryLogger _newMessageCallbackReactionFactoryLogger;
-    }
-
-    /// <summary>
-    /// Класс инкапсулирующий в себе логику логгирования фабрики создания реакции нового сообщения
-    /// </summary>
-    public class NewMessageCallbackReactionFactoryLogger
-    {
-        /// <summary>
-        /// Основной конструктор логгера для фабрики реакции нового сообщения
-        /// </summary>
-        /// <param name="logger">Используемый файловый логгер</param>
-        public NewMessageCallbackReactionFactoryLogger(IFileLogger logger)
-        {
-            _logger = logger;
-        }
-
-
-        /// <summary>
-        /// Метод логгирования информации о создании нового объекта
-        /// </summary>
-        public void LogInfoAboutCreatingObject()
-        {
-            _logger.WriteStringToLog("Создан объект фабрики реакции нового сообщения", LogLevel.Debug);
-        }
-
-        /// <summary>
-        /// Метод логгирования ошибки десериализации сообщения из json
-        /// </summary>
-        public void LogMessageDeserializeError()
-        {
-            _logger.WriteStringToLog("Неудалось дессериализовать message из Json", LogLevel.Error);
-        }
-
-        /// <summary>
-        /// Метод логгирования информации о начале создания реакции
-        /// </summary>
-        public void LogInfoAboutCreatingCallbackReaction()
-        {
-            _logger.WriteStringToLog("Фабрика создаёт реакцию нового сообщения", LogLevel.Debug);
-        }
-
-        /// <summary>
-        /// Метод логгирования информации о начале создания команды для реакции
-        /// </summary>
-        public void LogInfoAboutCreatingCallbackReactionCommand()
-        {
-            _logger.WriteStringToLog("Фабрика создаёт команду для реакции нового сообщения", LogLevel.Debug);
-        }
-
-
-        /// <summary>
-        /// Используемый файловый логгер
-        /// </summary>
-        private IFileLogger _logger;
     }
 }
---
+++ CallBackVKAPI/Logger/CallBackReactionFactoriesLoggers/NewMessageCallbackReactionFactoryLogger.cs
@@ -0,0 +1,20 @@
+﻿namespace CallBackVKAPI.Logger.CallBackReactionFactoriesLoggers
+{
+    /// <summary>
+    /// Класс, инкапсулирующий логику логгирования класса NewMessageCallbackReactionFactory
+    /// </summary>
+    public class NewMessageCallbackReactionFactoryLogger : CallBackReactionFactoryLogger
+    {
+        public NewMessageCallbackReactionFactoryLogger(IFileLogger logger) : base(logger)
+        {
+        }
+
+
+        /// <summary>
+        /// Метод, логгирующий информацию о начале создания команды для реакции
+        /// </summary>
+        public void LogCommandCreating() => 
+            FileLogger.WriteStringToLog("NewMessageCallbackReactionFactory запрашивает" +
+                " у менеджера комманду, для формирования реакции");
+    }
+}
